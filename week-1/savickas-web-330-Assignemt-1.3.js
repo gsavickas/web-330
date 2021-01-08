1ae341b5a6582cb3af7cff44e7904965201d0d55
Grayton Savickas <gsavickaspro@gmail.com>
2021-01-08 17:41:39 -0600

    adds new file names

---
+++ week-1/savickas-web-330-Assignemt-1.3.js
@@ -0,0 +1,16 @@
+/*
+============================================
+; Title:  Exercise 1.3 - Class Refresher
+; Author: Grayton Savickas
+; Date:   7 Jan 2021
+; Modified By: 
+; Description: Simulating classical programming in JavaScript 
+;===========================================
+*/
+const header = require('../week-1/Savickas-header')
+// Tests the import of header function works
+var headerValue = header.display("Grayton", "Savickas", "Exercise 1.3 - Class Refresher")
+console.log(headerValue);
+
+// Start of Program
+
