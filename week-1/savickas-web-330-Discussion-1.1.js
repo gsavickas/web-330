b87f7db9cd587929126b3224ed38f7c33092f10b
Grayton Savickas <gsavickaspro@gmail.com>
2021-01-08 19:11:56 -0600

    Adds assignment 1.3 and gitignore file

--- week-1/savickas-web-330-Discussion-1.1.js
+++ week-1/savickas-web-330-Discussion-1.1.js
@@ -1,5 +1,23 @@
+/*
+============================================
+; Title:  Exercise 1.1 - Class Refresher
+; Author: Grayton Savickas
+; Date:   7 Jan 2021
+; Modified By: 
+; Description: Simulating classical programming in JavaScript 
+;===========================================
+*/
+const header = require('../week-1/Savickas-header')
+// Tests the import of header function works
+var headerValue = header.display("Grayton", "Savickas", "Exercise 1.3 - Class Refresher")
+console.log(headerValue);
+
+// Start of Program
+
 function Tree(type, color, height){
     this.type = "spruce";
     this.color = "green";
     this.height = "2";
 }
+
+// End of program
